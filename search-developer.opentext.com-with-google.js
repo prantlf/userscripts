bb0e75be08db34a756d29e24f11201d41d68ea8c
Ferdinand Prantl <prantlf@gmail.com>
2014-06-19 13:30:51 +0200

    Add button to toggle between google and appworks searches

--- search-developer.opentext.com-with-google.js
+++ search-developer.opentext.com-with-google.js
@@ -1,10 +1,10 @@
 // ==UserScript==
 // @name       Search developer.opentext.com with Google
 // @namespace  mailto:fprantl@opentext.com
-// @version    0.1
+// @version    0.2
 // @description  Replaces the built-in search at developer.opentext.com by the
 //               Google Custom Search searching this domain only.  The URL points
-//               to my CSE but it can be modified.
+//               to my CSE but it can be modified.  Built-in search stays available.
 // @match      *://developer.opentext.com/*
 // @copyright  (c) 2014 OpenText GmbH
 // ==/UserScript==
@@ -15,8 +15,17 @@ if (typeof $ !== undefined) {
         parent = originalForm.parent();
     if (originalForm) {
       console.log('Replacing the AppWorks search with the Google custom search...');
-      $('<gcse:search>').insertAfter(originalForm);
-      originalForm.remove();
+      originalForm.hide();
+      var gcse = $('<gcse:search>').insertAfter(originalForm),
+          switcher = $('<button>')
+            .text('OTEX')
+            .addClass('btn switch-gcse')
+            .click(function () {
+              $('div#___gcse_0').toggle();
+              originalForm.toggle();
+              switcher.toggleClass('switch-gcse-otex')
+                .text(originalForm.is(':visible') ? 'GOOG' : 'OTEX');
+            }).insertBefore(originalForm);
       (function() {
         var cx = '006791522399351765273:14trghalcz4';
         var gcse = document.createElement('script');
@@ -30,11 +39,22 @@ if (typeof $ !== undefined) {
       $('style').html(
         'div#___gcse_0 {\n' +
         '  display: inline-block;\n' +
-        '  width: 25em  !important;\n' +
-        '  float: right  !important;\n' +
+        '  width: 25em !important;\n' +
+        '  float: right !important;\n' +
         '}\n' +
         'div.gsc-control-cse {\n' +
         '  padding: 0 !important;\n' +
+        '}\n' +
+        'button.switch-gcse {\n' +
+        '  float: right;\n' +
+        '  margin-left: 0.5ex;\n' +
+        '  margin-top: 2px !important;\n' +
+        '  font-size: 75%;\n' +
+        '  height: 27px;\n' +
+        '}\n' +
+        'button.switch-gcse.switch-gcse-otex {\n' +
+        '  margin-top: 5px !important;\n' +
+        '  height: 29px;\n' +
         '}\n'
       ).appendTo('head');
     }
